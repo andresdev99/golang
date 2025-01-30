2bcc9236a723c0facc470de798a5f6392c63571b
Andres Gutierrez <andresfelix951@gmail.com>
2025-01-30 12:31:50 -0500

    Adding go routines

--- main.go
+++ main.go
@@ -1,16 +1,5 @@
 package main
 
-type numberTypes interface {
-	~int | ~float32
-}
-
-func sum[T numberTypes](a, b T) T {
-	return a + b
-}
-
-type myAlias int
-
 func main() {
-	var a myAlias = 42
-	sum(a, 5)
+
 }
